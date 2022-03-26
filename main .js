1efa677a975ce9ce68910b5185b9986d51a1ad2a
stylevedant <93001991+stylevedant@users.noreply.github.com>
2022-03-26 19:01:00 +0530

    Add files via upload

---
+++ main .js
@@ -0,0 +1,36 @@
+prediction_1 = "";
+prediction_2 = "";
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attch('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHtml = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/DcoAQ8eTp/model.json',modelLoaded);
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first prediction is " + prediction_1;
+    speak_data_2 = "And the second prediction is " + prediction_2;
+    var utterThis = new SpeeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
